d43a2daaf7d86eff144cf1f96faa3c028f05489a
Per Mortensen <per@permortensen.com>
2021-05-16 09:18:31 +0200

    Show pronunciation

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 import "./app.css";
-import { fromAlBhed, toAlBhed } from "./translator";
+import { fromAlBhed, pronounce, toAlBhed } from "./translator";
 
 document.addEventListener("DOMContentLoaded", () => {
   const englishInput = document.querySelector("#english");
   const albhedInput = document.querySelector("#albhed");
+  const pronunciation = document.querySelector("#pronunciation");
 
   englishInput.addEventListener("input", (event) => {
     const translated = toAlBhed(event.target.value);
     albhedInput.value = translated;
+
+    const pronounced = pronounce(translated);
+    pronunciation.value = pronounced;
   });
 
   albhedInput.addEventListener("input", (event) => {
     const translated = fromAlBhed(event.target.value);
     englishInput.value = translated;
+
+    const pronounced = pronounce(event.target.value);
+    pronunciation.value = pronounced;
   });
 });
